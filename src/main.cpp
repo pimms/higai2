aad349b3aad5ea533ca60164c0ba9979f0dd07d5
pimms <joakim.stien@gmail.com>
2014-02-19 17:11:51 +0100

    Hey ho

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,28 @@
 #include "World.h"
 #include "Window.h"
 #include "Renderer.h"
+#include "PathNode.h"
+
+
+void InitializeWorld(World *world) {
+	int sx, sy;
+	world->GetSize(&sx, &sy);
+
+	for (int x=2; x < sx-2; x++) {
+		for (int y=2; y<sy-2; y++) {
+			world->GetNode(x, y)->SetType(PathNode::WALL);
+		}
+	}
+}
+
 
 
 int main() {
 	World world(20, 20);
+	InitializeWorld(&world);
+
 	Window window;
 	Renderer renderer(&window);
-
 
 	while (window.ShouldQuit() == false) {
 		window.HandleEvents();
