bd7f6ddb69fb011f49d27e3005b933560dcd3c1a
pimms <joakim.stien@gmail.com>
2014-02-19 13:02:08 +0100

    Added draw path function

--- src/Renderer.cpp
+++ src/Renderer.cpp
@@ -18,33 +18,42 @@ void Renderer::DrawWorld(World *world) {
 	for (int x = 0; x < sizeX; x++) {
 		for (int y = 0; y < sizeY; y++) {
 			PathNode *node = world->GetNode(x, y);
-			DrawPathNode(node, sizeX, sizeY);
+			DrawPathNode(world, node);
 		}
+	}
+}
+
+void Renderer::DrawPath(World *world, const list<PathNode*> &path) {
+	int sizeX, sizeY;
+	world->GetSize(&sizeX, &sizeY);
+	
+	list<PathNode*>::const_iterator cur = path.begin();
+	list<PathNode*>::const_iterator next = path.begin();
+	next++;
+	
+	while (next != path.end()) {
+		int x1, y1, x2, y2;
+		GetTileCoordinate(world, *cur, true, &x1, &y1);
+		GetTileCoordinate(world, *next, true, &x2, &y2);
+
+		SDL_RenderDrawLine(_window->GetRenderer(), x1, y1, x2, y2);
+
+		next++;
+		cur++;
 	}
 }
 
 
 /**** Private Methods *****/
-void Renderer::DrawPathNode(PathNode *node, int worldSizeX, int worldSizeY) {
+void Renderer::DrawPathNode(World *world, PathNode *node) {
 	// The screen coordinate of the node
 	int topX, topY;
 
-	// The grid indices of the node
-	int posX, posY;
-
 	// The size of the node
 	int sizeX, sizeY;
-
-	// The resolution of the screen
-	int resX, resY;
-
-	_window->GetResolution(&resX, &resY);
-	sizeX = resX/worldSizeX;
-	sizeY = resY/worldSizeY;
-
-	node->GetPosition(&posX, &posY);
-	topX = sizeX * posX;
-	topY = sizeY * posY;
+	
+	GetTileDimensions(world, &sizeX, &sizeY);
+	GetTileCoordinate(world, node, false, &topX, &topY);
 
 	// Draw a black 1px outline
 	SDL_SetRenderDrawColor(_window->GetRenderer(), 0, 0, 0, 255);
@@ -65,4 +74,33 @@ void Renderer::SetRenderColor(PathNode *node) {
 void Renderer::DrawRect(int x, int y, int w, int h) {
 	SDL_Rect rect = {x, y, w, h};
 	SDL_RenderDrawRect(_window->GetRenderer(), &rect);
+}
+
+
+void Renderer::GetTileDimensions(World *world, int *x, int *y) {
+	int sizeX, sizeY;
+	world->GetSize(&sizeX, &sizeY);
+	
+	int resX, resY;
+	_window->GetResolution(&resX, &resY);
+	
+	*x = resX / sizeX;
+	*y = resY / sizeY;
+}
+
+void Renderer::GetTileCoordinate(World *world, PathNode *node, 
+								bool center, int *x, int *y) {
+	int sizeX, sizeY;
+	int posX, posY;
+
+	world->GetSize(&sizeX, &sizeY);
+	node->GetPosition(&posX, &posY);
+	
+	*x = sizeX * posX; 
+	*y = sizeY * posY;
+
+	if (center) {
+		*x += sizeX / 2;
+		*y += sizeY / 2;
+	}
 }
--- src/Renderer.h
+++ src/Renderer.h
@@ -1,6 +1,9 @@
 #pragma once
 
 #include <SDL2/SDL.h>
+#include <list>
+
+using std::list;
 
 // Forward declarations
 class Window;
@@ -13,12 +16,24 @@ public:
 	Renderer(Window *Window);
 	
 	void DrawWorld(World *world);
+	void DrawPath(World *world, const list<PathNode*> &path);
 
 private:
-	void DrawPathNode(PathNode *node, int worldSizeX, int worldSizeY);
+	void DrawPathNode(World *world, PathNode *node);
 	
 	void SetRenderColor(PathNode *node);
 	void DrawRect(int x, int y, int w, int h);
+	
+	/* Assigns the pixel size of each tile to W and H.
+	 */
+	void GetTileDimensions(World *world, int *w, int *h);
+
+	/* Assigns the screen coordinates of the path node "node" to X and Y.
+	 * If center is true, the center of the tile is assigned to the variables.
+	 * Otherwise, the top left position is assigned.
+	 */
+	void GetTileCoordinate(World *world, PathNode *node, 
+							bool center, int *x, int *y);
 
 	Window *_window;
 };
