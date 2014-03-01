3fcd3b054e669a8ae5383b5f32cfe56678a1f488
pimms <joakim.stien@gmail.com>
2014-03-01 17:25:10 +0100

    Better doc in this file

--- src/PathCreator.h
+++ src/PathCreator.h
@@ -10,9 +10,19 @@ public:
 	PathCreator(World *world);
 	~PathCreator();
 
+	/* Find a path between _curA and _curB, the previously
+	 * used nodes. This method should only be called if the
+	 * world has changed, as it will return the exact same
+	 * path otherwise.
+	 */
 	Path* FindPath();
+
+	/* Finds a path between A and B.
+	 */
 	Path* FindPath(PathNode *a, PathNode *b);
 
+	/* Return the path found most recently.
+	 */
 	Path* GetPath() const;
 
 private:
